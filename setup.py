8ba950f4b5dc4bc68bfe28b08f53f308d77d2dbd
David Pretty <david.pretty@anu.edu.au>
2013-01-10 16:44:01 +1100

    install requires numpy also

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ execfile('h1/version.py')
 setup(name='h1',
       version=__version__,
       packages=['h1'],
-      install_requires=['scipy'],
+      install_requires=['numpy', 'scipy'],
       )
